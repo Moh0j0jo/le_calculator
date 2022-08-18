158e2a802caa6067fef7ee950faba7e39e5695a6
Moh0j0jo <pinnel.peter@gmail.com>
2022-08-18 17:08:51 +0200

    calculator style

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-
-
-function genNumpad(size) {
-
-    let numpad = document.querySelector(".numpad1");
-    numpad.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-    numpad.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-
-    for (let i = 0; i < size; i++) {
-        let numDiv = document.createElement("div");
-        let btn = document.createElement("input");
-        btn.type = 'button';
-        btn.TEXT_NODE = i;
-        btn.id = 'Submit';
-        btn.className = 'numbers';
-
-        numpad.insertAdjacentElement("beforeend", numDiv.appendChild(btn));
-
-    }
-}
-
-genNumpad(3)
-
 function add(params) {
 
 };

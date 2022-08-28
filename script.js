3164c21ae17be23813499666c656a851399f8002
Moh0j0jo <pinnel.peter@gmail.com>
2022-08-28 17:17:31 +0200

    js code refactoring

--- script.js
+++ script.js
@@ -1,82 +1,91 @@
 let input = "";
-let number = 0;
-let tempNumber = 0;
+let number = "";
+let tempNumb = "";
 let ArrNumb = [];
+let ArrTemp = [];
 let operator = "";
+let solution = "";
 
 let display1 = document.getElementById("story");
 let display2 = document.getElementById("numbdisp");
 
-function dispDel(){
 
-  display1.innerText = " ";
-  display2.innerText = " ";
-  
+function numbers(value, operator) {
+  if (!operator) {
+    ArrNumb.push(value);
+    number = ArrNumb.join("");
+    display1.innerText=number;
+  } else {
+    ArrTemp.push(value);
+    tempNumb = ArrTemp.join("");
+    display1.innerText=tempNumb;
+  };
 };
 
-function display(input){
-  //console.log(input)
-  display1.innerText = input;
-}; 
 
 
-function calculator(){
-  switch(this.value){
+function reset() {
+  number = "";
+  tempNumb = "";
+  ArrNumb = [];
+  ArrTemp = [];
+  operator = "";
+};
+
+function add(){
+  return Number(number) + Number(tempNumb);
+};
+
+function multiple(){
+  return Number(number) * Number(tempNumb);
+};
+
+function calculate(){
+  switch(operator){
     case "+":
-      break;
-    case "-":
+      solution =add();
+      
       break;
     case "✕":
+      solution = multiple();
       break;
-    case "÷":
-      break;
-    case "±":
-      break;
-    default:
+    
+      default:
       break;
   }
+  display2.innerText=solution;
 };
 
-
-function validate(){
+function validate() {
 
   switch (this.className) {
-    
+
     case "number":
-      if (number!=""){
-        ArrNumb.push(this.value);
-        number = ArrNumb.join("");
-        display(number);
-      } else {
-        tempNumber = number;
-        number = 0;
-        ArrNumb = [];
-        ArrNumb.push(this.value);
-        number = ArrNumb.join("");
-        display(number);
-      };
+      numbers(this.value, operator);
       break;
 
     case "operator":
-        display(this.value);
-        calculator(this.value);
+      operator = this.value
+      display1.innerText=operator;
       break;
 
     case "delete":
-     
-      dispDel;
+
       break;
-    
+
+    case "calculate":
+      calculate();
+      reset();
+      break;
+
     default:
 
       break;
   }
 };
 
-
-
 let clsButton = document.getElementsByTagName('button');
- 
-  for (var i = 0; i < clsButton.length; i++) {
-    clsButton[i].addEventListener('click', validate, false);
-  }
+
+for (var i = 0; i < clsButton.length; i++) {
+  clsButton[i].addEventListener('click', validate, false);
+}
